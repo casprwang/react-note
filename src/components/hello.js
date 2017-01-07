bcc41cb66bf8270c844cc14ed830737bb84041bc
wangsongiam <wangsongiam@gmail.com>
2017-01-07 16:20:49 -0500

    🎁login interface

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,14 +1,43 @@
 import React from 'react';
+// import RactDOM from 'react-dom';
+import NoteCard from './notesCard';
 
 class Hello extends React.Component {
   constructor() {
     super();
     this.state = {
       notes: [],
+      loggedin: false,
     };
     this.showSidebar = this.showSidebar.bind(this);
     this.addNote = this.addNote.bind(this);
+    this.removeNote = this.removeNote.bind(this);
+    this.showCreate = this.showCreate.bind(this);
+    this.createUser = this.createUser.bind(this);
+    this.showLogin = this.showLogin.bind(this);
+    this.loginUser = this.loginUser.bind(this);
   }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        firebase.database().ref().on('value', (res) => {
+            // console.log(res.val());
+          const userData = res.val();
+          const dataArray = [];
+            // console.log(userData);
+          for (const objkey in userData) {
+            userData[objkey].key = objkey;
+            dataArray.push(userData[objkey]);
+          }
+          this.setState({
+            notes: dataArray,
+          });
+        });
+      }
+    });
+  }
+
 
   showSidebar(e) {
     e.preventDefault();
@@ -21,11 +50,63 @@ class Hello extends React.Component {
       title: this.noteTitle.value,
       text: this.noteText.value,
     };
-    const newNotes = Array.from(this.state.notes);
-    newNotes.push(note);
-    this.setState({
-      notes: newNotes,
-    });
+
+    firebase.database().ref().push(note);
+    this.noteTitle.value = '';
+    this.noteText.value = '';
+    this.showSidebar(e);
+  }
+
+  removeNote(noteId) {
+    firebase.database().ref(noteId).remove();
+  }
+
+  createUser(e) {
+    e.preventDefault();
+    // Check the passwords match
+    // if so we want to create a user
+    const password = this.createPassword.value;
+    const confirm = this.confirmPassword.value;
+    const email = this.createEmail.value;
+    if (password === confirm) {
+      firebase.auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then((res) => {
+          this.showCreate(e);
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    } else {
+      alert('Password must match');
+    }
+  }
+
+  showCreate(e) {
+    e.preventDefault();
+    this.overlay.classList.toggle('show');
+    this.createUserModal.classList.toggle('show');
+  }
+
+  showLogin(e) {
+    e.preventDefault();
+    this.overlay.classList.toggle('show');
+    this.loginModal.classList.toggle('show');
+  }
+
+  loginUser(e) {
+    e.preventDefault();
+    const email = this.userEmail.value;
+    const password = this.userPassword.value;
+
+    firebase.auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((res) => {
+        this.showLogin(e);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
   }
 
   render() {
@@ -34,14 +115,14 @@ class Hello extends React.Component {
         <header className="mainHeader">
           <h1>Note it!</h1>
           <nav><a href="" onClick={this.showSidebar} >Add New Note</a></nav>
+          <nav><a href="" onClick={this.showCreate}>Create Account</a></nav>
+          <nav><a href="" onClick={this.showLogin}>Login</a></nav>
         </header>
+        <div className="overlay" ref={(ref) => { this.overlay = ref; }} />
         <section className="notes">
-          <div className="noteCard">
-            <i className="fa fa-edit" />
-            <i className="fa fa-times" />
-            <h4>Test note!</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
+          {this.state.notes.map((note, i) =>
+            <NoteCard key={`note-${i}`} note={note} removeNote={this.removeNote} />,
+          )}
         </section>
         <aside className="sidebar" ref={(ref) => { this.sidebar = ref; }}>
           <form onSubmit={this.addNote}>
@@ -55,13 +136,58 @@ class Hello extends React.Component {
             <input type="submit" value="Add New Note" />
           </form>
         </aside>
+
+        {/* login overlay */}
+        <div className="loginModal modal" ref={(ref) => { this.loginModal = ref; }}>
+          <div className="close" onClick={this.showLogin}>
+            <i className="fa fa-times" />
+          </div>
+          <form action="" onSubmit={this.loginUser}>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input type="text" name="email" ref={(ref) => { this.userEmail = ref; }} />
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input type="password" name="password" ref={(ref) => { this.userPassword = ref; }} />
+            </div>
+            <div>
+              <input type="submit" value="Login" />
+            </div>
+          </form>
+        </div>
+
+        {/* create user overlay */}
+        <div className="createUserModal modal" ref={(ref) => { this.createUserModal = ref; }} >
+          <div className="close" onClick={this.showCreate}>
+            <i className="fa fa-times" />
+          </div>
+          {/* <form action="" onSubmit={e => this.createUser.call(this.e)} > */}
+          <form action="" onSubmit={this.createUser} >
+            <div>
+              <label htmlFor="createEmail">Email:</label>
+              <input type="text" name="createEmail" ref={(ref) => { this.createEmail = ref; }} />
+            </div>
+            <div>
+              <label htmlFor="createPassword">Create Password:</label>
+              <input type="password" name="createPassword" ref={(ref) => { this.createPassword = ref; }} />
+            </div>
+            <div>
+              <label htmlFor="confirmPassword">Confirm Password:</label>
+              <input type="password" name="confirmPassword" ref={(ref) => { this.confirmPassword = ref; }} />
+            </div>
+            <div>
+              <input type="submit" value="Create" />
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
-}
+          }
 
 
-// Hello.propTypes = {
+          // Hello.propTypes = {
 //   message: React.PropTypes.string,
 // };
 
---
+++ src/components/notesCard.js
@@ -0,0 +1,66 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+export default class NoteCard extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      editing: false,
+      note: {},
+    };
+    this.save = this.save.bind(this);
+  }
+
+  // componentDidMount() {
+  //   this.setState({
+  //     note: this.props.note,
+  //   });
+  // }
+
+  save(e) {
+    e.preventDefault();
+    // console.log(this);
+    const dbRef = firebase.database().ref(this.props.note.key);
+
+    dbRef.update({
+      title: this.noteTitle.value,
+      text: this.noteText.value,
+    });
+
+    this.setState({
+      editing: false,
+    });
+  }
+
+  render() {
+    let editingTemp = (
+      <span>
+        <h4>{this.props.note.title}</h4>
+        <p>{this.props.note.text}</p>
+      </span>
+    );
+
+    if (this.state.editing) {
+      editingTemp = (
+        <form onSubmit={this.save}>
+          <div>
+            <input type="text" defaultValue={this.props.note.title} name="title" ref={(ref) => { this.noteTitle = ref; }} />
+          </div>
+          <div>
+            <input type="text" defaultValue={this.props.note.text} ref={(ref) => { this.noteText = ref; }} />
+          </div>
+          <input type="submit" value="Done editing" />
+        </form>
+      );
+    }
+
+    return (
+      <div className="noteCard" >
+        <i className="fa fa-edit" onClick={() => this.setState({ editing: true })} />
+        <i className="fa fa-times" onClick={() => this.props.removeNote(this.props.note.key)} />
+        {editingTemp}
+      </div>
+    );
+  }
+}
